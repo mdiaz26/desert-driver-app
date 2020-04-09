b80296b79b99ebb1b034a94e589435d9c302af97
mdiaz26 <mike.diaz006@gmail.com>
2020-04-09 19:56:16 -0400

    two paths working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import {Navbar, Login, SignUp} from './components'
+import {Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar/>
+      <Switch>
+        <Route path="/login" component={Login}/>
+        <Route path="/signup" component={SignUp}/>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import GameStats from './GameStats'
+import Login from './Login'
+import Navbar from './Navbar'
+import ScoreRow from './ScoreRow'
+import SignUp from './SignUp'
+
+export {
+    GameStats,
+    Login,
+    Navbar,
+    ScoreRow,
+    SignUp
+}
