4487834600c09eafbf58bbecff9dce51cdd414c0
github username <github email address>
2020-04-12 16:26:37 -0400

    changed game layout

--- src/components/GameStats.js
+++ src/components/GameStats.js
@@ -6,25 +6,32 @@ class GameStats extends Component {
 
     return (
       <div className="stats-container">
+          <div className="lives">
+            <div>
+                <img className="current-player-image" src={this.props.stats.playerAvatar} />
+                <h3 className="player-name">Cain Firstman</h3>
+            </div>
+            <h4 >X {this.props.stats.lives}</h4>
+          </div>
         <div className="stats-div">
           <div className="pause-button">
             
           </div>
-          <div className="lives">
-            <img className="current-player-image" src={this.props.stats.playerAvatar} />
-            <h4 >X {this.props.stats.lives}</h4>
-          </div>
+          
           <div className="score">
-            <h5>Score: {Math.round(this.props.stats.score)}</h5>
+            <h5>Score <h4>{Math.round(this.props.stats.score)}</h4></h5>
+            
           </div>
           <div className="high-score">
-            <h5>High Score: {this.props.stats.highScore}</h5>
+            <h5>High Score <h4>{this.props.stats.highScore}</h4></h5>
+           
           </div>
           <div className="distance">
-            <h5>Distance: {(this.props.stats.distance).toFixed(2)}</h5>
+            <h5>Distance <h4>{(this.props.stats.distance).toFixed(2)}</h4></h5>
+            
           </div>
           <div className="timer">
-            <h5>Timer: {(this.props.stats.timer)}</h5>
+            <h5>Timer <h4>{(this.props.stats.timer)}</h4></h5>
           </div>
         </div>
       </div>
