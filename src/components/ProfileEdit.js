f7cd0dbcdd8a59239773c141c65f0dbdb21d0df6
mdiaz26 <mike.diaz006@gmail.com>
2020-04-13 11:02:06 -0400

    added conditional modal to confirm deletion

--- src/components/ProfileEdit.js
+++ src/components/ProfileEdit.js
@@ -1,18 +1,37 @@
 import React from 'react'
+import DeleteModal from './DeleteModal'
 
-const ProfileEdit = (props) => {
-    return(
-        <div>
-            <form onSubmit={props.handleSubmit}>
-                <label>
-                    Username:
-                    <input type="text" name="username" value={props.user.username} onChange={props.handleChange}/>
-                </label>
-                <input type="submit" value="Submit"/>
-            </form>
-            <button onClick={props.deleteAccount}>Delete Account</button>
-        </div>
-    )
+class ProfileEdit extends React.Component {
+    
+    state = {
+        modalDisplay: false
+    }
+
+    toggleModal = () => {
+        this.setState(prevState => ({
+            modalDisplay: !prevState.modalDisplay
+        }))
+    }
+
+    render(){
+        return(
+            <div>
+                <form onSubmit={this.props.handleSubmit}>
+                    <label>
+                        Username:
+                        <input type="text" name="username" value={this.props.user.username} onChange={this.props.handleChange}/>
+                    </label>
+                    <input type="submit" value="Submit"/>
+                </form>
+                <button onClick={this.toggleModal}>Delete Account</button>
+                {this.state.modalDisplay && 
+                    <DeleteModal 
+                        toggleModal={this.toggleModal} 
+                        deleteAccount={this.props.deleteAccount}
+                    />}
+            </div>
+        )
+    }
 }
 
 export default ProfileEdit
